4fecbcc3f8e86795eb5a4f7c3a71d7849c508db1
Michael Vinyard <47393421+mvinyard@users.noreply.github.com>
2022-05-20 21:35:40 -0600

    Update __init__.py

--- flexinet/_models/__init__.py
+++ flexinet/_models/__init__.py
@@ -5,3 +5,6 @@ __email__ = ", ".join(["vinyard@g.harvard.edu",])
 
 
 from ._LinearVAE import _LinearVAE as LinearVAE
+
+from ._compose_nn_sequential import _NeuralNetwork as NN
+from ._compose_nn_sequential import _compose_nn_sequential as compose_nn_sequential
